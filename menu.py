cbd8ceeb76b896dfa67fc4310e05059d9e18d7f2
Kishan <k.sharma8321@gmail.com>
2021-03-26 17:50:28 +0000

    added window class that scales to fill a container

--- menu.py
+++ menu.py
@@ -6,15 +6,21 @@ from window_sizing import Window
 def menu(screen):
     clock = pygame.time.Clock()
     pygame.display.set_caption("Menu Screen")
-    square = Window((10, 10))
+
+    window = Window((100, 100), GREEN, (0.9, 0.9), (0.5, 0.5), screen)
+    pygame.event.post(pygame.event.Event(pygame.VIDEORESIZE, {'size': (500, 300), 'w': 500, 'h': 300}))
 
     while True:
         screen.fill(RED)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return screen
             if event.type == pygame.VIDEORESIZE:
                 screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+                window.resize()
+
+        window.draw()
 
         pygame.display.update()
         clock.tick(30)
--- window_sizing.py
+++ window_sizing.py
@@ -2,5 +2,24 @@ import pygame
 
 
 class Window:
-    def __init__(self, size: tuple):
+    def __init__(self, size: tuple, color, scale: tuple, pos: tuple, parent):
         self.image = pygame.Surface(size)
+        self.rect = self.image.get_rect()
+        self.image.fill(color)
+
+        self.parent = parent
+        self.scale = scale
+        self.pos = pos
+
+    def resize(self):
+        # scale image a fraction of the parents size
+        self.rect.width = self.parent.get_width() * self.scale[0]
+        self.rect.height = self.parent.get_height() * self.scale[1]
+        self.image = pygame.transform.scale(self.image, self.rect.size)
+
+        # align the center of the rect a fraction of the parents size, relative to the parent
+        self.rect.centerx = self.parent.get_width() * self.pos[0]
+        self.rect.centery = self.parent.get_height() * self.pos[1]
+    def draw(self):
+        self.parent.blit(self.image, (self.rect.x, self.rect.y))
+
